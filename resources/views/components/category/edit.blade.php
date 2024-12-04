d1311f1db2eba2e8a99cd3c22d165e165185baf3
nijwel <nijwel@yahoo.com>
2024-12-04 14:28:54 +0600

    category create and edit form design change

--- resources/views/components/category/edit.blade.php
+++ resources/views/components/category/edit.blade.php
@@ -1,28 +1,31 @@
-<h2>Create Category</h2>
-@if (session()->has('message'))
-    <div class="alert alert-success">
-        {{ session('message') }}
+<div class="card w-50 mx-auto">
+    <div class="card-header d-flex justify-content-between content-center">
+        <h2>Edit Category</h2>
+        <button type="button" class="btn btn-sm btn-primary mt-3" wire:click="changeView('list')">Back</button>
     </div>
-@endif
-<form wire:submit.prevent="update">
-    <div class="form-group mt-3">
-        <label for="name">Name</label>
-        <input type="text" class="form-control" id="name" placeholder="Enter name" wire:model="name">
-        @error('name')
-            <span class="text-danger">{{ $message }}</span>
-        @enderror
+    <div class="card-body">
+
+        <form wire:submit.prevent="update">
+            <div class="form-group mt-3">
+                <label for="name">Name</label>
+                <input type="text" class="form-control" id="name" placeholder="Enter name" wire:model="name">
+                @error('name')
+                    <span class="text-danger">{{ $message }}</span>
+                @enderror
+            </div>
+            <div class="form-group mt-3">
+                <label for="status">Status</label>
+                <select class="form-control" id="status" wire:model.live="status">
+                    <option value="">Select status</option>
+                    <option value="active">Active</option>
+                    <option value="inactive">Inactive</option>
+                </select>
+                @error('status')
+                    <span class="text-danger">{{ $message }}</span>
+                @enderror
+            </div>
+            <button type="submit" class="btn btn-sm btn-primary mt-3">Save</button>
+            <button type="button" class="btn btn-sm btn-secondary mt-3" wire:click="changeView('list')">Cancel</button>
+        </form>
     </div>
-    <div class="form-group mt-3">
-        <label for="status">Status</label>
-        <select class="form-control" id="status" wire:model.live="status">
-            <option value="">Select status</option>
-            <option value="active">Active</option>
-            <option value="inactive">Inactive</option>
-        </select>
-        @error('status')
-            <span class="text-danger">{{ $message }}</span>
-        @enderror
-    </div>
-    <button type="submit" class="btn btn-sm btn-primary mt-3">Save</button>
-    <button type="button" class="btn btn-sm btn-secondary mt-3" wire:click="changeView('list')">Cancel</button>
-</form>
+</div>
